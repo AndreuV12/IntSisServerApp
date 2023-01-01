382c8d75c8d84bcf8f40a7ec76da6a82c29580a5
AndreuV12 <andreuvillaro12@gmail.com>
2023-01-01 18:54:59 +0100

    cambis

--- config/config.js
+++ config/config.js
@@ -8,6 +8,6 @@ let GOOGLE_SECRET = process.env.GOOGLE_SECRET
 let PORT = process.env.PORT
 
 let URL = process.env.ON_PRODUCTION ? process.env.URL : "http://localhost:3030/"
-let COOKIE_DOMAIN = process.env.ON_PRODUCTION ? "heroku.com" : "localhost"
+let COOKIE_DOMAIN = process.env.ON_PRODUCTION ? ".herokuapp.com" : "localhost"
 
 export  {DB_USER, DB_PASSWORD, GOOGLE_CLIENT, GOOGLE_SECRET, PORT, URL, COOKIE_DOMAIN}
