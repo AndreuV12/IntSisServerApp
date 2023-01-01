42746b98cafdb1e988b93156e3ad074f2170336e
AndreuV12 <andreuvillaro12@gmail.com>
2023-01-01 18:21:42 +0100

    cambis

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv'
-dotenv.config({ path: 'config/.env' })
+dotenv.config({ path: 'config/.env', debug: true })
 
 let DB_USER = process.env.DB_USER
 let DB_PASSWORD = process.env.DB_PASSWORD
 let GOOGLE_CLIENT = process.env.GOOGLE_CLIENT
 let GOOGLE_SECRET = process.env.GOOGLE_SECRET
 let PORT = process.env.PORT
-let URL = process.env.URL
+
+let URL = process.env.ON_PRODUCTION ? process.env.URL : "http://localhost:3030/"
+let COOKIE_DOMAIN = process.env.ON_PRODUCTION ? process.env.URL : "localhost"
+console.log(DB_USER, DB_PASSWORD, GOOGLE_CLIENT, GOOGLE_SECRET, PORT, URL)
 export  {DB_USER, DB_PASSWORD, GOOGLE_CLIENT, GOOGLE_SECRET, PORT, URL}
